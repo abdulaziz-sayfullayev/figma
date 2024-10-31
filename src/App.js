9e05d446c0f0f5a147376aa62b2d35c6b5ea4b2c
Abdulaziz-Middle <sayfullayevabdulazizbek@gmail.com>
2024-10-31 21:10:12 +0500

    figma navbar 100% zo'r ishlitdi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Pages from "./Pages";
 import "./App.css";
 function App() {
   return (
-    <div>
+    <div className='Figma'>
       <Navbar/>
       <Pages/>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { IoCallSharp } from "react-icons/io5";
+
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div>
-      <h1>logo</h1>
+      <section className="nav-section">
+        <div className="NavigationBar">
+          <div className="content01">
+            <div className="LogoDiv">
+              <span>Itrechos</span>
+            </div>
+            <div className="MenuCall">
+              <div className={`Menu ${isMobileMenuOpen ? "active" : ""}`}>
+                <ul className="nav-links">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Services</a>
+                  </li>
+                  <li>
+                    <a href="#">Pages</a>
+                  </li>
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="Call">
+                <a className="CallA" href="tel:+998912800562">
+                  <IoCallSharp />
+                  +998912800562
+                </a>
+              </div>
+              <div className="menu-toggle" onClick={toggleMobileMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
+
 export default Navbar;
